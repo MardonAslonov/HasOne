3a48beb8f81ddbb8df46c8939a645627a8677a04
Mardon <aslonovmardon1995@gmail.com>
2023-01-21 22:52:55 +0500

    Added a new route

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $directors = \App\Models\Director::all();
+
+    foreach($directors as $director){
+        echo 'Director name: '.$director['name'].'<br>';
+    }
+    
 });
